41271530b8bf6c26cd50a05f0f7644841cdaee4a
unknown <erik.argel.ungs@gmail.com>
2024-07-02 15:45:02 -0300

    Se importa componente footer al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    FooterComponent
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
