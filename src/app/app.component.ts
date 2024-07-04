ad6745ee849035dc13b27711b1c72e9ede1f3e80
unknown <erik.argel.ungs@gmail.com>
2024-07-04 18:14:49 -0300

    Agregar landing component al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @Component({
   standalone: true,
@@ -9,7 +10,8 @@ import { NavbarComponent } from './components/layout/navbar/navbar.component';
     NavbarComponent,
     RouterOutlet, 
     RouterLink, 
-    RouterLinkActive
+    RouterLinkActive,
+    LandingComponent
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
