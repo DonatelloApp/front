ad3146eeda12e2dc1c6fb1e0cfb8524c0b6563c2
unknown <erik.argel.ungs@gmail.com>
2024-07-09 05:29:24 -0300

    Crear phone input component

---
+++ src/app/components/common/phoneInput/phone-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+
+declare var intlTelInput: any;
+
+@Component({
+  selector: 'app-phone-input',
+  standalone: true,
+  templateUrl: './phone-input.component.html',
+  styleUrls: ['./phone-input.component.scss']
+})
+export class PhoneInputComponent {
+  @Output() phoneChanged = new EventEmitter<string>();
+
+  ngAfterViewInit() {
+
+    const input = document.querySelector("#phoneInput") as HTMLInputElement;
+    const iti = intlTelInput(input, {
+      initialCountry: 'auto',
+      separateDialCode: true,
+      containerClass: "phoneInputContainer",
+      utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@23.1.0/build/js/utils.js"
+    });
+
+    input.addEventListener('change', () => {
+      this.phoneChanged.emit(iti.getNumber());
+    });
+
+  }
+}
