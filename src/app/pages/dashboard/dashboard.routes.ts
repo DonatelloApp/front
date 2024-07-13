a0add548679b8865d060e01bac286b6e5c1679e1
unknown <erik.argel.ungs@gmail.com>
2024-07-13 05:50:53 -0300

    Agregar ruta para la pagina userProfile

--- src/app/pages/dashboard/dashboard.routes.ts
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const DASHBOARD_ROUTES: Routes = [];
+export const DASHBOARD_ROUTES: Routes = [
+    {
+        path: 'user-profile',
+        loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
+    },
+];
