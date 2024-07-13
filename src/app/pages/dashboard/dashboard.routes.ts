ccde568ef0b7b2d4d10203605efaed70dca79ea0
unknown <erik.argel.ungs@gmail.com>
2024-07-13 19:53:17 -0300

    Agregar comentarios a rutas dashboard

--- src/app/pages/dashboard/dashboard.routes.ts
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -3,18 +3,22 @@ import { Routes } from '@angular/router';
 export const DASHBOARD_ROUTES: Routes = [
     {
         path: '',
+        //modificar ruta componente cuando se termine
         loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
     },
     {
         path: 'stock',
+        //modificar ruta componente cuando se termine
         loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
     },
     {
         path: 'finance',
+        //modificar ruta componente cuando se termine
         loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
     },
     {
         path: 'suppliers',
+        //modificar ruta componente cuando se termine
         loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
     },
     {
