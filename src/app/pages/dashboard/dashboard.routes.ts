ef9ce99f8f212728d885b8da4c34000415b3ad64
unknown <erik.argel.ungs@gmail.com>
2024-07-13 19:45:45 -0300

    Agregar rutas dashboard

--- src/app/pages/dashboard/dashboard.routes.ts
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -2,6 +2,22 @@ import { Routes } from '@angular/router';
 
 export const DASHBOARD_ROUTES: Routes = [
     {
+        path: '',
+        loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
+    },
+    {
+        path: 'stock',
+        loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
+    },
+    {
+        path: 'finance',
+        loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
+    },
+    {
+        path: 'suppliers',
+        loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
+    },
+    {
         path: 'user-profile',
         loadComponent: () => import('./userProfile/userProfile.component').then(m => m.UserProfileComponent),
     },
