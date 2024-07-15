36c2024e6d9df0e0acee1766ce93aeb69d5edbad
unknown <erik.argel.ungs@gmail.com>
2024-07-15 17:23:13 -0300

    Configurar Auth guard

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -14,6 +14,7 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./pages/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+    canActivate: [authGuard]
   },
   {
     path: '**',
