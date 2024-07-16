5e131146d3f1a84fec7d5eba3fd7e6bb12bf3791
unknown <erik.argel.ungs@gmail.com>
2024-07-16 19:26:25 -0300

    Cambiar nombres formcontrols para coincidir con variables del model

--- src/app/components/form-proveedores/form-proveedores.component.ts
+++ src/app/components/form-proveedores/form-proveedores.component.ts
@@ -1,34 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { Proveedor } from 'src/app/core/models/proveedor';
+import { capitalizeWords } from 'src/app/utils/utils';
 
 @Component({
   selector: 'app-form-proveedores',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-proveedores.component.html',
-  styleUrls: ['./form-proveedores.component.scss']
+  styleUrls: ['./form-proveedores.component.scss'],
 })
 export class FormProveedoresComponent implements OnChanges {
-
   @Input() proveedor: Proveedor | null = null;
   @Output() save = new EventEmitter<Proveedor>();
 
   formulario: FormGroup;
 
   constructor(private fb: FormBuilder) {
-
     this.formulario = this.fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(3)]],
-      empresa: ['', [Validators.required]],
-      contacto: ['', [Validators.required]],
-      telefono: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      companyName: ['', [Validators.required]],
+      lastContact: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
     });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    
     if (changes['proveedor'] && this.proveedor) {
       this.formulario.patchValue(this.proveedor);
     }
@@ -36,14 +46,16 @@ export class FormProveedoresComponent implements OnChanges {
 
   onSubmit() {
     if (this.formulario.valid) {
+      const proveedorReq: Proveedor = {
+        name: capitalizeWords(this.formulario.value.name),
+        phone: this.formulario.value.phone,
+        companyName: '',
+        lastContact: '',
+      };
 
-      const proveedorData: Proveedor = {
-        ...this.proveedor,
-        ...this.formulario.value
-      }
+      if (this.proveedor) proveedorReq.id = this.proveedor.id;
 
-      this.save.emit(this.formulario.value);
+      this.save.emit(proveedorReq);
     }
   }
-
 }
