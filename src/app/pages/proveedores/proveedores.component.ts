03549931cb281f3340385bc11edfe32e7b88b279
unknown <erik.argel.ungs@gmail.com>
2024-07-16 19:33:07 -0300

    Implementar utilizacion del servicio de proveedores

--- src/app/pages/proveedores/proveedores.component.ts
+++ src/app/pages/proveedores/proveedores.component.ts
@@ -1,55 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProveedoresService } from './../../core/services/proveedores.service'
+import { ProveedoresService } from './../../core/services/proveedores.service';
 import { Proveedor } from 'src/app/core/models/proveedor';
 import { FormProveedoresComponent } from 'src/app/components/form-proveedores/form-proveedores.component';
 import { ModalComponent } from 'src/app/components/common/modal/modal.component';
-import { NavbarComponent } from "../../components/layout/navbar/navbar.component";
-import { SidebarComponent } from "../../components/layout/sidebar/sidebar.component";
+import { NavbarComponent } from '../../components/layout/navbar/navbar.component';
+import { SidebarComponent } from '../../components/layout/sidebar/sidebar.component';
+import { LoginService } from 'src/app/core/services/login.service';
 
 @Component({
   selector: 'app-proveedores',
-  standalone:true,
-  imports: [CommonModule, ReactiveFormsModule, FormProveedoresComponent, ModalComponent, NavbarComponent, SidebarComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormProveedoresComponent,
+    ModalComponent,
+    NavbarComponent,
+    SidebarComponent,
+  ],
   templateUrl: './proveedores.component.html',
-  styleUrls: ['./proveedores.component.scss']
+  styleUrls: ['./proveedores.component.scss'],
 })
-export class ProveedoresComponent implements OnInit{
-
-  proveedores:Proveedor[]=[];
+export class ProveedoresComponent implements OnInit {
+  private loginService = inject(LoginService);
+  private provService = inject(ProveedoresService);
+  userLoginOn: boolean = false;
+  proveedores: Proveedor[] = [];
   selectedProveedor: Proveedor | null = null;
- 
+  errorMessage: String = '';
 
-  constructor(private provService: ProveedoresService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.loadProveedores();
+    this.provService.getProveedores().subscribe({
+      next: (userData) => {
+        this.proveedores = userData;
+      },
+      error: (errorData) => {
+        this.errorMessage = errorData;
+      },
+      complete: () => {
+        console.info('Suppliers loaded successfully');
+      },
+    });
   }
 
-  loadProveedores(){
-    this.proveedores = this.provService.getProveedores();
-  }
-
-  onEditProveedor( proveedor: Proveedor){
+  onEditProveedor(proveedor: Proveedor) {
     this.selectedProveedor = proveedor;
   }
 
-  onNewProveedor(){
+  onNewProveedor() {
     this.selectedProveedor = null;
   }
 
-  onSaveProveedor(proveedor : Proveedor){
-    if(proveedor.id){
-      this.provService.modificarProveedor(proveedor);
-      this.loadProveedores();
-      this.selectedProveedor=null;
-    
-    }else{
-      this.provService.postProveedor(proveedor);
-      this.loadProveedores();
-      this.selectedProveedor=null;
+  onSaveProveedor(proveedor: Proveedor) {
+    if (proveedor.id) {
+      this.provService.updateProveedor(proveedor).subscribe({
+        error: (errorData) => {
+          this.errorMessage = errorData;
+        },
+        complete: () => {
+          console.info('Supplier successfully updated');
+        },
+      });
+      this.selectedProveedor = null;
+    } else {
+      this.provService.addProveedor(proveedor).subscribe({
+        error: (errorData) => {
+          this.errorMessage = errorData;
+        },
+        complete: () => {
+          console.info('Supplier successfully added');
+        },
+      });
+      this.selectedProveedor = null;
     }
   }
-  
 }
