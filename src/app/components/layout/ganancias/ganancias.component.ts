42ff1866b5158b9d46bad103ed2e45a6c4d4ca0c
DamianL96 <damianlambrecht@gmail.com>
2024-07-25 14:35:10 -0300

    limite de gastos

--- src/app/components/layout/ganancias/ganancias.component.ts
+++ src/app/components/layout/ganancias/ganancias.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/core/models/Transaction';
+import { FinanzasService } from 'src/app/core/services/finanzas.service';
 
 @Component({
   selector: 'app-ganancias',
   standalone:true,
+  imports:[CurrencyPipe],
   templateUrl: './ganancias.component.html',
   styleUrls: ['./ganancias.component.scss']
 })
-export class GananciasComponent {
+export class GananciasComponent implements OnInit{
 
+  ingresoDelMes: Transaction[]=[];
+  gastoDelMes: Transaction[]=[];
+  totalGastoDelMes:number = 0;
+  totalIngresoDelMes:number = 0;
+
+  ganancias:number =0;
+
+  constructor(private finanzasService: FinanzasService){}
+
+  ngOnInit(): void {
+    this.loadIngresoDelMes();
+    this.loadGastosDelMes();
+    this.calcularGanancia();
+  }
+
+  loadIngresoDelMes(){
+    this.finanzasService.getIngresosDelMes().subscribe({
+      next:(data)=> this.ingresoDelMes = data,
+      error:(error)=> console.error('Error cargando ingresos del mes:', error)
+    });
+  }
+
+  loadGastosDelMes(){
+    this.finanzasService.getGastosdelMes().subscribe({
+      next:(data)=> this.gastoDelMes = data,
+      error:(error)=> console.error('Error cargando gastos del mes:', error)
+    });
+  }
+
+  calcularGanancia(){
+    
+    this.ingresoDelMes.forEach( ing=> {
+      this.totalIngresoDelMes += ing.amount;
+    });
+
+    this.gastoDelMes.forEach( gasto=> {
+      this.totalGastoDelMes += gasto.amount;
+    })
+
+    this.ganancias = this.totalIngresoDelMes - this.totalGastoDelMes;
+
+  }
 }
--- src/app/components/layout/limite-gastos/limite-gastos.component.ts
+++ src/app/components/layout/limite-gastos/limite-gastos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/core/models/Transaction';
+import { FinanzasService } from 'src/app/core/services/finanzas.service';
 
 @Component({
   selector: 'app-limite-gastos',
@@ -6,6 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './limite-gastos.component.html',
   styleUrls: ['./limite-gastos.component.scss']
 })
-export class LimiteGastosComponent {
+export class LimiteGastosComponent implements OnInit{
+
+  limiteDeGasto = 200000;
+  totalGastoDelMes = 0;
+  porcentaje :number =0;
+  gastosDelMes : Transaction[]=[];
+
+  constructor(private finanzasService: FinanzasService){}
+
+  ngOnInit(): void {
+    this.loadGastosDelMes();
+    //this.calcularGasto()
+  }
+
+  loadGastosDelMes(){
+    this.finanzasService.getGastosdelMes().subscribe({
+      next:(data)=> {
+        this.gastosDelMes = data;
+        this.calcularGasto();
+      },
+      error:(error)=> console.error('Error cargando gastos del mes:', error)
+    });
+  }
+
+  calcularGasto(){
+    this.gastosDelMes.forEach( gasto=> {
+      this.totalGastoDelMes += gasto.amount;
+    });
+    this.totalGastoDelMes = this.totalGastoDelMes / 1000;
+    this.calcularPorcentaje();
+  }
+
+  calcularPorcentaje(){
+    this.porcentaje = (this.totalGastoDelMes / (this.limiteDeGasto / 1000) ) * 100;
+  }
 
 }
