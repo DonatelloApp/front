f9641dc9ca4fdbc867ea793b8cb7d9df91b46808
DamianL96 <damianlambrecht@gmail.com>
2024-07-26 17:29:44 -0300

    env urlApi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   urlHost: 'https://i002-donatello-back-java-latest-z9hn.onrender.com/',
-  urlApi: 'http://127.0.0.1:8080/api',
+  urlApi: 'https://i002-donatello-back-java-latest-z9hn.onrender.com/',
 };
