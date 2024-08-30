dd4108d73728babbd6fa0c8ca797c0798ff7690b
unknown <erik.argel.ungs@gmail.com>
2024-08-30 18:33:35 -0300

    cambiar url deploy back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  urlHost: 'https://i002-donatello-back-java-latest-z9hn.onrender.com/',
-  urlApi: 'https://i002-donatello-back-java-latest-z9hn.onrender.com/',
+  urlHost: 'https://backjavadonatelloapp.onrender.com/',
+  urlApi: 'https://backjavadonatelloapp.onrender.com/api',
 };
